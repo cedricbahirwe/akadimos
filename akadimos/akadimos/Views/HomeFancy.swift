0df234c6e83c51bd8f80d8e46714b3f99e94df14
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-02-25 03:56:05 +0200

    Add Home

---
+++ akadimos/akadimos/Views/HomeFancy.swift
@@ -0,0 +1,18 @@
+//
+//  HomeFancy.swift
+//  akadimos
+//
+//  Created by Cédric Bahirwe on 25/02/2024.
+//
+
+import SwiftUI
+
+struct HomeFancy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeFancy()
+}
