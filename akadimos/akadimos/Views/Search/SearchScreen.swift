255d3b4b307a23b1da78a7c9fb41a888217cde36
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-02-27 02:22:22 +0200

    Final UI Fixes'

--- akadimos/akadimos/Views/Search/SearchScreen.swift
+++ akadimos/akadimos/Views/Search/SearchScreen.swift
@@ -7,69 +7,43 @@
 
 import SwiftUI
 
-private struct FilterData {
-    var locationName: String = ""
-    var locationRadius: Double = 0
-    let locationRange = 0...10_000.0
-
-    var estimatedPrice: Double = 0
-    let priceRange = 10_000...1_000_000.0
-    
-    var rating: Int = 0
-    
-    static let `default` = FilterData(locationName: "Kigali City, Convention Center, KG 182", locationRadius: 6_000, estimatedPrice: 200_000, rating: 3)
-
-}
 struct SearchScreen: View {
     @State private var searchEntry = "Calapan"
+    @State private var isSearching = false
     @State private var showFilter = false
-    
     @State private var filterData: FilterData = .default
+    private var showFilterResults: Bool {
+        !showFilter && (filterData != FilterData.default)
+    }
     
     var body: some View {
         List {
-            VStack(alignment: .leading) {
-                Text("Filtered Results")
-                    .font(.title3)
-                    .fontWeight(.medium)
-                
-                Label(
-                    title: { Text("Kigali City, Convention Center, KG 182")},
-                    icon: { Image.locationCircle }
-                )
-                .labelStyle(MiniLabeledStyle())
-                
-                Label(
-                    title: { Text("RWF 10,000 - RWF 1,000,000") },
-                    icon: { Image.moneySquare }
-                )
-                
-                HStack(spacing: 20) {
-                    Label(
-                        title: { Text("6 kms") },
-                        icon: { Image.discoverCircle }
+            
+            if showFilterResults {
+                FilterResultsView(result: filterData)
+                    .listRowBackground(
+                        Color.clear
+                            .background(
+                                BackgroundStyle.background
+                                    .shadow(.inner(color: .offBlackShadow, radius: 15, x: 10, y: 10))
+                                    .shadow(.inner(color: .offWhiteShadow, radius: 15, x: -10, y: -10))
+                            )
                     )
-                    
-                    Label(
-                        title: { Text("3 stars") },
-                        icon: { Image.normalStar }
-                    )
-                }
+                    .transition(.opacity)
             }
-            .labelStyle(MiniLabeledStyle())
-            .font(.callout)
-            .frame(maxWidth: .infinity, alignment: .leading)
             
             ForEach(1..<20) { i in
                 SearchItemRow(i: i)
-                    .listRowInsets(EdgeInsets())
-                    .listRowBackground(Color.clear)
+            }
+            .listRowInsets(EdgeInsets())
+            .listRowBackground(
+                Color.clear
                     .background(
                         .background
                             .shadow(.inner(color: .offBlackShadow, radius: 15, x: 10, y: 10))
                             .shadow(.inner(color: .offWhiteShadow, radius: 15, x: -10, y: -10))
                     )
-            }
+            )
         }
         .listSectionSpacing(020)
         .listRowSpacing(20)
@@ -84,19 +58,43 @@ struct SearchScreen: View {
                     Spacer()
                     Image.filterGlass
                         .onTapGesture {
-                            showFilter.toggle()
+                            withAnimation {
+                                showFilter.toggle()
+                            }
                         }
                 }
+                
+                VStack(alignment: .leading) {
+                    SearchField("", text: $searchEntry)
+                        .accessoryAction(.filterMagnify) {
+                            withAnimation {
+                                isSearching.toggle()
+                            }
+                        }
+                    
+                    if isSearching {
+                        Text("Showing 3 results for \"\(searchEntry)\"")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .transition(
+                                .asymmetric(
+                                    insertion: .push(from: .top),
+                                    removal: .push(from: .bottom)
+                                )
+                            )
 
-                SearchField("", text: $searchEntry)
-                    .accessoryAction(.filterMagnify) { }
+                    }
+                }
                     .padding(.vertical, 10)
             }
             .padding(.horizontal)
             .background(.primaryBackground.opacity(0.95))
         }
         .sheet(isPresented: $showFilter) {
-            FilterView(filter: filterData) {
+            FilterView(filter: filterData) { newData in
+                withAnimation {
+                    self.filterData = newData
+                }
                 showFilter = false
             }
             .presentationDetents([.height(520)])
@@ -116,7 +114,7 @@ private extension SearchScreen {
                         .scaledToFit()
                         .frame(width: 60, height: 60)
                         .background(.ultraThinMaterial, in: .circle)
-                        
+                    
                     VStack(alignment: .leading) {
                         Text("Placeholder")
                             .fontWeight(.medium)
@@ -148,12 +146,12 @@ private extension SearchScreen {
     
     struct FilterView: View {
         @State var filter: FilterData
-        var onFilter: () -> Void
+        var onFilter: (FilterData) -> Void
         
         var body: some View {
             VStack(alignment: .leading, spacing: 10) {
                 Text("Location").font(.title2).bold()
-                    
+                
                 TextField("Type a location", text: $filter.locationName)
                     .padding(10)
                     .frame(maxWidth: .infinity, alignment: .leading)
@@ -165,7 +163,7 @@ private extension SearchScreen {
                         Text("Distance").font(.title2).bold()
                         Spacer()
                         
-                        Text("\((filter.locationRadius/1000).formatted())kms")
+                        Text(filter.locationRadiusText)
                             .padding(9)
                             .background(.regularMaterial, in: .rect(cornerRadius: 4))
                     }
@@ -179,13 +177,12 @@ private extension SearchScreen {
                 .tint(
                     Gradient(colors: [.green, .orange, .red])
                 )
-
+                
                 VStack(alignment: .leading) {
                     Text("Price Range").font(.title2).bold()
-
+                    
                     Slider(value: $filter.estimatedPrice, in: filter.priceRange)
                 }
-                    
                 
                 HStack  {
                     Text("RWF \(filter.priceRange.lowerBound.formatted())")
@@ -200,14 +197,14 @@ private extension SearchScreen {
                         .background(.regularMaterial, in: .rect(cornerRadius: 4))
                     
                 }
-    
+                
                 VStack(alignment: .leading, spacing: 10) {
                     Text("Ratings").font(.title2).bold()
                     RatingView(value: $filter.rating)
                 }
-                                
+                
                 Button {
-                    onFilter()
+                    onFilter(filter)
                 } label: {
                     Text("Filter")
                         .font(.title3)
@@ -227,6 +224,76 @@ private extension SearchScreen {
             )
         }
     }
+    
+    struct FilterResultsView: View {
+        let result: FilterData
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text("Filtered Results")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                
+                Label(
+                    title: { Text(result.locationName)},
+                    icon: { Image.locationCircle }
+                )
+                .labelStyle(MiniLabeledStyle())
+                
+                Label(
+                    title: { Text(result.priceRangeText) },
+                    icon: { Image.moneySquare }
+                )
+                
+                HStack(spacing: 20) {
+                    Label(
+                        title: { Text(result.locationRadiusText) },
+                        icon: { Image.discoverCircle }
+                    )
+                    
+                    Label(
+                        title: { Text("\(result.rating) stars") },
+                        icon: { Image.normalStar }
+                    )
+                }
+            }
+            .labelStyle(MiniLabeledStyle())
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+    
+}
+
+
+fileprivate struct MiniLabeledStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack(alignment: .top, spacing: 8) {
+            configuration.icon
+            configuration.title
+        }
+    }
+}
+
+private struct FilterData: Equatable {
+    var locationName: String = ""
+    var locationRadius: Double = 0
+    let locationRange = 0...10_000.0
+    
+    var estimatedPrice: Double = 0
+    let priceRange = 10_000...1_000_000.0
+    
+    var rating: Int = 0
+    
+    var priceRangeText: String {
+        "RWF \(priceRange.lowerBound.formatted()) - RWF \(priceRange.upperBound.formatted())"
+    }
+    
+    var locationRadiusText: String {
+        "\((locationRadius/1000).formatted())kms"
+    }
+    
+    static let `default` = FilterData(locationName: "Kigali City, Convention Center, KG 182", locationRadius: 6_000, estimatedPrice: 200_000, rating: 3)
+    
 }
 
 #Preview("SearchScreen") {
@@ -238,76 +305,5 @@ private extension SearchScreen {
 }
 
 #Preview("FilterView", traits: .sizeThatFitsLayout) {
-    SearchScreen.FilterView(filter: .default, onFilter: {})
-}
-
-///     Menu {
-///         // A multi select palette
-///         ControlGroup {
-///             ForEach(ColorTags.allCases) { colorTag in
-///                 Toggle(isOn: $selectedColorTags[colorTag]) {
-///                     Label(colorTag.name, systemImage: "circle")
-///                 }
-///                 .tint(colorTag.color)
-///             }
-///         }
-///         .controlGroupStyle(.palette)
-///         .paletteSelectionEffect(.symbolVariant(.fill))
-///
-///         // A momentary / stateless palette
-///         ControlGroup {
-///             ForEach(Emotes.allCases) { emote in
-///                 Button {
-///                     sendEmote(emote)
-///                 } label: {
-///                     Label(emote.name, systemImage: emote.systemImage)
-///                 }
-///             }
-///         }
-///         .controlGroupStyle(.palette)
-///     }
-///
-
-
-struct MiniLabeledStyle: LabelStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        HStack(alignment: .top, spacing: 8) {
-            configuration.icon
-            configuration.title
-        }
-    }
-}
-
-struct RatingView: View {
-    // TODO: - Create Custom Binding & Interaction
-    @Binding private var rating: Int
-    init(value: Binding<Int>) {
-        // Determine the initial value for _rating based on the condition
-        var initialValue = min(max(0, value.wrappedValue), 5)
-
-        // Initialize _rating with the determined initial value
-        self._rating = Binding(
-            get: { initialValue },
-            set: { newValue in
-                value.wrappedValue = newValue
-            }
-        )
-    }
-    var body: some View {
-        HStack(spacing: 5) {
-            ForEach(0..<5) { i in
-                Image.magicStar
-                    .renderingMode(.template)
-                    .foregroundStyle(i < rating ? .blue : .primary)
-                    .overlay {
-                        if i >= rating {
-                            Image.magicStar
-                                .renderingMode(.template)
-                                .scaleEffect(0.8)
-                                .foregroundStyle(.background)
-                        }
-                    }
-            }
-        }
-    }
+    SearchScreen.FilterView(filter: .default, onFilter: { _ in })
 }
